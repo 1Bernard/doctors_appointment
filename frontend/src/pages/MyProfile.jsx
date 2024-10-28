f08c77cd1cbea12b30761d8e7a38794869ba548a
Bernard Owusu Ansah <90481075+1Bernard@users.noreply.github.com>
2024-10-28 11:13:51 +0000

    add login

--- frontend/src/pages/MyProfile.jsx
+++ frontend/src/pages/MyProfile.jsx
@@ -1,8 +1,27 @@
 import React from 'react'
+import { useState } from 'react'
+import {assets} from '../assets/assets'
 
 const MyProfile = () => {
+  const [userData, setUserData] = useState({
+    name: "Edward Vincent",
+    image: assets.profile_pic,
+    email: 'richardjames@gmail.com',
+    phone: '1234567890',
+    address: {
+      line1: "57th Cross, Richmond",
+      line2: "Circle, Church Road, London"
+    },
+    gender: 'Male',
+    dob: '1990-01-01',
+  })
+
+  const [isEdit, setIsEdit] = useState(false)
   return (
-    <div></div>
+    <div>
+      <img src={userData.image} alt="" />
+
+    </div>
   )
 }
 
