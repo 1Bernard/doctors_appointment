5bc64441c6e9f14f8f7bce34d13a22b9604093cb
Bernard Owusu Ansah <90481075+1Bernard@users.noreply.github.com>
2024-11-02 18:36:26 +0000

    add admin authentication middleware

--- backend/server.js
+++ backend/server.js
@@ -22,4 +22,4 @@ app.get('/', (req, res) => {
   res.send('API WORKING')
 })
 
-app.listen(port, ()=> console.log("Server started", port))
+app.listen(port, ()=> console.log("Server started", port)) 
