229eac437b07533f166ae744cd6b18dd0195959c
Bernard Owusu Ansah <90481075+1Bernard@users.noreply.github.com>
2024-11-04 13:42:13 +0000

    add login

---
+++ admin/src/components/Navbar.jsx
@@ -0,0 +1,17 @@
+import React, { useContext } from 'react'
+import { assets } from '../assets/assets'
+import { AdminContext } from '../context/AdminContext'
+
+const Navbar = () => {
+  const {aToken} = useContext(AdminContext)
+  return (
+    <div>
+      <div>
+        <img src={assets.admin_logo} alt="" />
+        <p>{aToken ? 'Admin' : 'Doctor'}</p>
+      </div>
+    </div>
+  )
+}
+
+export default Navbar
