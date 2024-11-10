a30e9eacec8e908ef91c7f4a371550264c4ad9c8
Bernard Owusu Ansah <90481075+1Bernard@users.noreply.github.com>
2024-11-10 11:31:28 +0000

    add book appointments

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -4,6 +4,7 @@ import userModel from '../models/userModel.js'
 import jwt from 'jsonwebtoken'
 import {v2 as cloudinary} from 'cloudinary'
 import doctorModel from '../models/doctorModel.js'
+import appointmentModel from '../models/appointmentModel.js'
 
 // API to register user
 const registerUser = async (req, res) => {
@@ -110,7 +111,7 @@ const updateProfile = async (req, res) => {
 }
 
 // API to book appointment
-const bookAppointment = async () => {
+const bookAppointment = async (req, res) => {
   try {
     const { userId, docId, slotDate, slotTime } = req.body
     const docData = await doctorModel.findById(docId).select('-password')
@@ -143,8 +144,8 @@ const bookAppointment = async () => {
       userData,
       docData,
       amount: docData.fees,
-      slotDate,
       slotTime,
+      slotDate,
       date: Date.now()
     }
 
